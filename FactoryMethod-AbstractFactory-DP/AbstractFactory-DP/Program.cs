c40af5fd6e7f58d692a57c9544417c83949858c1
Sanan Akhverdiev <120374801+sakhverdiev@users.noreply.github.com>
2023-12-03 15:41:58 +0400

    first commit

---
+++ FactoryMethod-AbstractFactory-DP/AbstractFactory-DP/Program.cs
@@ -0,0 +1,92 @@
+﻿interface IChair
+{
+    void hasLeg();
+    void sitOn();
+}
+
+interface ICoffeTable
+{
+    // ...
+}
+
+interface ISofa
+{
+    // ...
+}
+
+
+class VictorianChair : IChair
+{
+    public void hasLeg()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void sitOn()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+
+class ModernChair : IChair
+{
+    public void hasLeg()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void sitOn()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+
+
+
+
+interface IFurnitureFactory
+{
+    IChair createChair();
+    ICoffeTable createCoffeTable();
+    ISofa createSofa();
+}
+
+
+class VictorianFurnitureFactory : IFurnitureFactory
+{
+    public IChair createChair()
+    {
+        throw new NotImplementedException();
+    }
+
+    public ICoffeTable createCoffeTable()
+    {
+        throw new NotImplementedException();
+    }
+
+    public ISofa createSofa()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+
+class ModernFurnitureFactory : IFurnitureFactory
+{
+    public IChair createChair()
+    {
+        throw new NotImplementedException();
+    }
+
+    public ICoffeTable createCoffeTable()
+    {
+        throw new NotImplementedException();
+    }
+
+    public ISofa createSofa()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ FactoryMethod-AbstractFactory-DP/FactoryMethod-DP/Program.cs
@@ -0,0 +1,53 @@
+﻿abstract class Dialog
+{
+    public void render()
+    {
+        Button okButton = createButton();
+        okButton.onClick();
+        okButton.render();
+    }
+
+    public virtual Button createButton()
+    {
+        return null;
+    }
+
+}
+
+interface Button
+{
+    void render();
+    void onClick();
+
+}
+
+
+class WindowsDialog : Dialog
+{
+    public override Button createButton()
+    {
+        return new WindowsButton();
+    }
+}
+
+class WebDialog : Dialog
+{
+    public override Button createButton()
+    {
+        return new WindowsButton();
+    }
+}
+
+class WindowsButton : Button
+{
+    public void onClick() { }
+
+    public void render() { }
+}
+
+class HTMLButton : Button
+{
+    public void onClick() {  }
+
+    public void render() { }
+}
